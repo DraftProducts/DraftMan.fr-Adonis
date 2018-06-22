40abecde640efab5bd6ca8b1ca0e86bb88e8971a
DraftMan <nicovanaarsen@gmail.com>
2018-06-22 10:04:19 +0200

    Hooks Setup

---
+++ start/hooks.js
@@ -0,0 +1,15 @@
+const { hooks } = require('@adonisjs/ignitor')
+const Remarkable = require('remarkable');
+markdown = new Remarkable();
+
+hooks.after.providersRegistered(() => {
+  const View = use('View')
+
+  View.global('trim', (text, value = 800) => {
+    return text.substring(0, value)
+  })
+
+  View.global('markdown', (text) => {
+    return markdown.render(text)
+  })
+})
