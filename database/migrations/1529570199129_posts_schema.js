d177a499c8cb9ff4a5301bd0390099cdaa4b9e61
DraftMan <nicovanaarsen@gmail.com>
2018-06-22 10:09:28 +0200

    Post migrations created

---
+++ database/migrations/1529570199129_posts_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostsSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.string('title')
+      table.string('url')
+      table.text('description')
+      table.string('tags')
+      table.text('content')
+      table.date('published_at') // -> Si rempli = publié et ta la date en plus
+      table.timestamps() // created_at - updated_at
+      table.integer('author_id').notNullable() // -> ID de l'auteur
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostsSchema
