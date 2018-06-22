118c742c383c095c0feffbaa334926c95a723a6c
DraftMan <nicovanaarsen@gmail.com>
2018-06-22 10:05:21 +0200

    Seeds initiatlized

---
+++ database/seeds/PostSeeder.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| PostSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Factory = use('Factory')
+const Database = use('Database')
+
+class PostSeeder {
+  async run () {
+    // Deletes all data in those tables
+    await Database.truncate('users')
+    await Database.truncate('posts')
+
+    // Generates & Stores a user
+    const user = await Factory.model('App/Models/User').create()
+
+    // Generates 10 posts
+    const posts = await Factory.model('App/Models/Blog').makeMany(10)
+
+    // Assigns those posts to the user
+    await user.posts().saveMany(posts)
+  }
+}
+
+module.exports = PostSeeder
