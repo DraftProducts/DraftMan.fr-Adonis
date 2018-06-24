06e379d064664f0a42584e167d66f886da1be2f0
DraftMan <nicovanaarsen@gmail.com>
2018-06-24 14:43:42 +0200

    Contact Mail système finish

--- app/Controllers/Http/ContactController.js
+++ app/Controllers/Http/ContactController.js
@@ -11,40 +11,41 @@ class ContactController {
     async verify ({ request, session, response }) {
 
         const messages = {
+            'author.required': 'Veuillez indiquer votre identité.',
             'email.required': 'Veuillez entrer une adresse email valide.',
             'objet.required': 'Veuillez entrer l\'objet de votre mail.',
-            'author.required': 'Veuillez indiquer votre identité.',
-            'author.required': 'Veuillez indiquer un message a envoyer.'
+            'message.required': 'Veuillez indiquer un message a envoyer.',
+            'commentconnu.required': 'Veuillez indiquer comment vous avez connu DraftMan.'
         }
 
         const rules = {
+          author: 'required',
           email: 'required|email',
           objet: 'required',
           message: 'required',
-          author: 'required',
+          commentconnu: 'required',
         }
     
         const validation = await validate(request.all(), rules, messages)
 
         if (validation.fails()) {
-            return validation.messages()
+            session
+            .withErrors(validation.messages())
+            .flashExcept()
+
+            return response.redirect('back')
         }
     
-        return 'Validation passed'
-    }
-    async sendMail ({ request }) {
         const email_to = 'contact@draftman.fr';
         const data = request.only(['email', 'objet', 'author','twitter','discord','commentconnu','message'])
-        const user = await User.create(data)
     
-        await Mail.send('mails/contact', user.toJSON(), (message) => {
+        await Mail.send('mails/contact', data, (message) => {
           message
-            .to(user.email)
-            .from('author-<email>')
+            .to(email_to)
+            .from('<email>', '<author>')
             .subject('<objet>')
+            .replyTo('<email>', '<author>')
         })
-    
-        return 'Mail send'
     }
 }
 
