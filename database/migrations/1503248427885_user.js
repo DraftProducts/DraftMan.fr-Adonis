d1ab0784d488dbfdee70e8ff1fa783ccc470dd9c
DraftMan <nicovanaarsen@gmail.com>
2018-06-25 15:18:58 +0200

    users database migration created

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -10,7 +10,6 @@ class UserSchema extends Schema {
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
-    
     })
   }
 
