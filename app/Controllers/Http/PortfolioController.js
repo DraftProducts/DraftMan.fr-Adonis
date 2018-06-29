09ef890599103d846dde17a7941faafd6b720e29
DraftMan <nicovanaarsen@gmail.com>
2018-06-29 22:52:40 +0200

    async opti

--- app/Controllers/Http/PortfolioController.js
+++ app/Controllers/Http/PortfolioController.js
@@ -5,14 +5,14 @@ const Portfolio = use('App/Models/Portfolio')
 class PortfolioController {
     async index({ view }){
 
-        const portfolio = await (Portfolio.all()).toJSON();
+        const portfolio = (await Portfolio.all()).toJSON();
 
         return view.render('portfolio.index', {portfolio: portfolio})
     }
 
     async details({params, view}){
 
-        const project = await (Portfolio.find(params.id)).toJSON();
+        const project = (await Portfolio.find(params.id)).toJSON();
 
         return view.render('portfolio.details', {project})
     }
