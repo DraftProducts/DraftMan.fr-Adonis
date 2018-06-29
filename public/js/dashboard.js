683598006ff0c360f32ef6f43c48b95e6c64b39b
DraftMan <nicovanaarsen@gmail.com>
2018-06-29 22:51:56 +0200

    public files created for the panel dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,22 +1,35 @@
-const scrolling = document.getElementById('menu');
-const products_list = document.getElementsByClassName('content')[0];
-
-window.addEventListener('scroll', function () {
-    const bottom = products_list.offsetTop + products_list.offsetHeight;
-    const margin = products_list.offsetHeight - scrolling.offsetHeight;
-    const scrollTop = window.scrollY;
-    if (scrollTop > document.getElementById('become').offsetHeight) {
-        if (scrollTop > (bottom - 98)) {
-            scrolling.style.marginTop = margin + "px";
-            scrolling.style.position = 'static';
-            return;
+const dashboard = new Vue({
+    el: '#dashboard',
+    data: {
+        open: true,
+    },
+    mounted(){
+        this.addEvents(this);
+    },
+    methods: {
+        addEvents(vm) {
+            window.addEventListener('scroll', function() {
+                const bottom = vm.$refs.products_list.offsetTop + vm.$refs.products_list.offsetHeight;
+                const margin = vm.$refs.products_list.offsetHeight - vm.$refs.scrolling.offsetHeight;
+                const scrollTop = window.scrollY;
+                if (scrollTop > 98) {
+                    if (scrollTop > ((bottom - vm.$refs.scrolling.clientHeight) - 60)) {
+                        vm.$refs.scrolling.style.marginTop = margin + "px";
+                        vm.$refs.scrolling.style.position = 'static';
+                        return;
+                    }
+                    vm.$refs.scrolling.style.position = 'fixed';
+                    vm.$refs.scrolling.style.top = '60px';
+                    vm.$refs.scrolling.style.marginTop = 'auto';
+                    return;
+                }
+                vm.$refs.scrolling.style.position = 'static';
+                vm.$refs.scrolling.style.top = '0px';
+                vm.$refs.scrolling.style.marginTop = 'auto';
+            });
+        },
+        toggleMenu: function(){
+            this.open = !this.open
         }
-        scrolling.style.position = 'fixed';
-        scrolling.style.top = '98px';
-        scrolling.style.marginTop = 'auto';
-        return;
     }
-    scrolling.style.position = 'static';
-    scrolling.style.top = '0px';
-    scrolling.style.marginTop = 'auto';
 });
