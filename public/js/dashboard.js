06dab6a89d6b6745fc1ad7eb22c360bf3a321204
DraftMan <nicovanaarsen@gmail.com>
2018-07-02 22:22:59 +0200

    refs modified & input filled added

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -9,23 +9,34 @@ const dashboard = new Vue({
     methods: {
         addEvents(vm) {
             window.addEventListener('scroll', function() {
-                const bottom = vm.$refs.products_list.offsetTop + vm.$refs.products_list.offsetHeight;
-                const margin = vm.$refs.products_list.offsetHeight - vm.$refs.scrolling.offsetHeight;
+                const bottom = vm.$refs.corps.offsetTop + vm.$refs.corps.offsetHeight;
+                const margin = vm.$refs.corps.offsetHeight - vm.$refs.content.offsetHeight;
                 const scrollTop = window.scrollY;
                 if (scrollTop > 98) {
-                    if (scrollTop > ((bottom - vm.$refs.scrolling.clientHeight) - 60)) {
-                        vm.$refs.scrolling.style.marginTop = margin + "px";
-                        vm.$refs.scrolling.style.position = 'static';
+                    if (scrollTop > ((bottom - vm.$refs.content.clientHeight) - 60)) {
+                        vm.$refs.content.style.marginTop = margin + "px";
+                        vm.$refs.content.style.position = 'static';
                         return;
                     }
-                    vm.$refs.scrolling.style.position = 'fixed';
-                    vm.$refs.scrolling.style.top = '60px';
-                    vm.$refs.scrolling.style.marginTop = 'auto';
+                    vm.$refs.content.style.position = 'fixed';
+                    vm.$refs.content.style.top = '60px';
+                    vm.$refs.content.style.marginTop = 'auto';
                     return;
                 }
-                vm.$refs.scrolling.style.position = 'static';
-                vm.$refs.scrolling.style.top = '0px';
-                vm.$refs.scrolling.style.marginTop = 'auto';
+                vm.$refs.content.style.position = 'static';
+                vm.$refs.content.style.top = '0px';
+                vm.$refs.content.style.marginTop = 'auto';
+            });
+            const inputs = document.querySelectorAll("form div input")
+            inputs.forEach(input => {
+                if(input.value !== "")input.classList.add("filled")
+                input.addEventListener('change', () => {
+                    if(input.value === ""){
+                        input.classList.remove("filled")
+                    } else {
+                        input.classList.add("filled")
+                    }
+                })
             });
         },
         toggleMenu: function(){
