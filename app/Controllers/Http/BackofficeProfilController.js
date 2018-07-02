510b3402b04bcd22b88cde75e4de14ba6ee4441a
DraftMan <nicovanaarsen@gmail.com>
2018-07-02 22:28:30 +0200

    debug removed

--- app/Controllers/Http/BackofficeProfilController.js
+++ app/Controllers/Http/BackofficeProfilController.js
@@ -24,13 +24,12 @@ class BackofficeProfilController {
 
   async discordCallback({request,response}) {
     const code = request.get().code;
+    console.log(code)
     post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}`)
     .set('Authorization', `Basic ${cred}`)
-    .then(res => console.log(res.body.access_token))
+    .then(res => response.redirect(`/me/profile?token=${res.body.access_token}`))
     .catch(console.error);
   }
-
-  //     .then(res => response.redirect(`/me/profile?token=${res.body.access_token}`))
 
   async storeBasic({session, request, response}){
     const profil = (await User.query().where('username','=',session.get('username')).fetch()).first().toJSON();
