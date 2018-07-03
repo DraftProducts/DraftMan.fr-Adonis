8c157a511eceeaa726bedfd606e7b712e0f516f6
DraftMan <nicovanaarsen@gmail.com>
2018-07-03 21:09:36 +0200

    profil query to auth

--- app/Controllers/Http/BackofficeAccueilController.js
+++ app/Controllers/Http/BackofficeAccueilController.js
@@ -3,8 +3,8 @@
 const User = use('App/Models/User')
 
 class BackofficeAccueilController {
-  async index({view, session}) {
-    const profil = (await User.query().where('username','=',session.get('username')).fetch()).first().toJSON();
+  async index({view, session,auth}) {
+    const profil = auth.user.toJSON();
     return view.render('dashboard.accueil',{user: profil})
   }
 }
