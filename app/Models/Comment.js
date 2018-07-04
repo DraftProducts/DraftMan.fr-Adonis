a3385a3cad000f04d798fd91fed4803f831d27cd
DraftMan <nicovanaarsen@gmail.com>
2018-07-04 20:11:44 +0200

    reply added

--- app/Models/Comment.js
+++ app/Models/Comment.js
@@ -3,6 +3,9 @@
 const Model = use('Model')
 
 class Comment extends Model {
+    replies () {
+        return this.hasMany('App/Models/Comment', 'id', 'parent_id')
+    }
 }
 
 module.exports = Comment
