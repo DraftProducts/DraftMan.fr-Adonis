d4077ae3dd2bda06e22f0015561a4417a6059277
DraftMan <nicovanaarsen@gmail.com>
2018-07-08 13:53:17 +0200

    comments sys

--- app/Controllers/Http/BackofficeCommentsController.js
+++ app/Controllers/Http/BackofficeCommentsController.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const Comment = use('App/Models/Comment')
+
 class BackofficeCommentsController {
   async index({view,auth}) {
-    const user = auth.user.toJSON();
-    return view.render('dashboard.comments',{user})
+    const user = await auth.user.toJSON();
+    const comments = await Comment.query().with('post').where('seen','=',0).fetch()
+    return view.render('dashboard.comments',{user,comments: comments.toJSON()})
   }
 }
 
--- app/Models/Comment.js
+++ app/Models/Comment.js
@@ -6,6 +6,9 @@ class Comment extends Model {
     replies () {
         return this.hasMany('App/Models/Comment', 'id', 'parent_id')
     }
+    post () {
+        return this.belongsTo('App/Models/Post', 'id', 'post_id')
+    }
 }
 
 module.exports = Comment
