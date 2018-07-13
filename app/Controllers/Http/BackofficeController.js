d5897abb25395bf26f9058ff3aafc628d6c92fe6
DraftMan <nicovanaarsen@gmail.com>
2018-07-13 20:13:10 +0200

    minification

---
+++ app/Controllers/Http/BackofficeController.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const User = use('App/Models/User')
+const Comment = use('App/Models/Comment')
+
+class BackofficeController {
+  async index({view,auth}) {
+    const profil = auth.user.toJSON();
+    return view.render('dashboard.accueil',{user: profil})
+  }
+
+  async client({view,auth}) {
+    const profil = auth.user.toJSON();
+    return view.render('dashboard.client',{user: profil})
+  }
+
+  async comments({view,auth}) {
+    const user = await auth.user.toJSON();
+    const comments = (await Comment.query().with('post').where('seen','=',0).fetch()).toJSON()
+    return view.render('dashboard.comments',{user,comments})
+  }
+
+  async destroy_comment({params,response}) {
+    const comment = await Comment.find(params.id)
+    comment.seen = 1
+    await comment.save()
+    response.redirect('back')
+  }
+  
+  async valide_comment({params,response}) {
+    const comment = await Comment.find(params.id)
+    await comment.delete()
+    response.redirect('back')
+  }
+}
+
+module.exports = BackofficeController
