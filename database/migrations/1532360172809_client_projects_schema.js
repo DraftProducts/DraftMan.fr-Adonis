fa170ef8e992686c6ee0b121d22fc20292855f4e
DraftMan <nicovanaarsen@gmail.com>
2018-08-01 16:27:35 +0200

    migrations projects created

---
+++ database/migrations/1532360172809_client_projects_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ClientProjectsSchema extends Schema {
+  up () {
+    this.create('client_projects', (table) => {
+      table.increments()
+      table.string('name')
+      table.integer('total_payments')
+      table.integer('completed_payments')
+      table.string('folder')
+      table.integer('price')
+      table.integer('devblog_id')
+      table.string('user_id')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('client_projects')
+  }
+}
+
+module.exports = ClientProjectsSchema
---
+++ database/migrations/1532360982965_client_projects_devblog_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ClientProjectsDevblogSchema extends Schema {
+  up () {
+    this.create('client_projects_devblog', (table) => {
+      table.increments()
+      table.integer('client_project_id')
+      table.string('title')
+      table.string('description')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('client_projects_devblogs')
+  }
+}
+
+module.exports = ClientProjectsDevblogSchema
