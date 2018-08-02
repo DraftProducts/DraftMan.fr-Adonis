bd0786ec1c249c1cc8e5943b51d2c7617149ad58
DraftMan <nicovanaarsen@gmail.com>
2018-08-02 19:34:02 +0200

    sring > string

--- database/migrations/1529570199129_posts_schema.js
+++ database/migrations/1529570199129_posts_schema.js
@@ -11,7 +11,7 @@ class PostsSchema extends Schema {
       table.text('description')
       table.string('tags')
       table.text('content')
-      table.sring('image')
+      table.string('image')
       table.date('published_at') // -> Si rempli = publié et ta la date en plus
       table.timestamps() // created_at - updated_at
       table.integer('author_id').notNullable() // -> ID de l'auteur
