f5d71ae287a77630cddf9dd346e4739e3278d76e
DraftMan <nicovanaarsen@gmail.com>
2018-08-09 22:34:59 +0200

    relation details added

--- app/Models/Portfolio.js
+++ app/Models/Portfolio.js
@@ -6,6 +6,9 @@ class Portfolio extends Model {
     static get table () {
         return 'portfolio'
     }
+    details () {
+        return this.belongsTo('App/Models/PortfolioDetails', 'id', 'portfolio_details_id')
+    }
 }
 
 module.exports = Portfolio
