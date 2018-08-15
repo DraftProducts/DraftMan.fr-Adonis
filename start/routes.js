ffa1a7ddb811228eb83a4c2998e043db5eb5e4ac
DraftMan <nicovanaarsen@gmail.com>
2018-08-15 11:19:05 +0200

    All routes updates with the files moved

--- start/routes.js
+++ start/routes.js
@@ -21,13 +21,15 @@ const Route = use('Route')
  |--------------------------------------------------------------------------
  */
 
-Route.get('/', 'HomeController.index')
+Route.get('/', 'BlogController.accueil')
 
 Route.on('a-propos').render('a-propos')
 
 Route.on('discord').render('discord')
 
 Route.get('discord/login', 'ProfilController.discordLogin')
+
+Route.on('success').render('success')
 
 Route.on('draftbot').render('draftbot')
 
@@ -37,8 +39,8 @@ Route.on('draftbot').render('draftbot')
  |--------------------------------------------------------------------------
  */
 
-Route.get('/search', 'SearchController.index')
-Route.post('/search', 'SearchController.search')
+Route.get('search', 'SearchController.index')
+Route.post('search', 'SearchController.search')
 
 /**
  |--------------------------------------------------------------------------
@@ -46,10 +48,10 @@ Route.post('/search', 'SearchController.search')
  |--------------------------------------------------------------------------
  */
 
-Route.get('blog', 'PostController.index')
-Route.get('blog/list', 'PostController.list')
-Route.get('blog/:id-:link', 'PostController.show')
-Route.post('blog/:id/comment', 'PostController.comment')
+Route.get('blog', 'BlogController.index')
+Route.get('blog/list', 'BlogController.list')
+Route.get('blog/:id-:link', 'BlogController.show')
+Route.post('blog/:id/comment', 'BlogController.comment')
 
 /**
  |--------------------------------------------------------------------------
@@ -66,8 +68,8 @@ Route.get('portfolio/:id', 'PortfolioController.show')
  |--------------------------------------------------------------------------
  */
 
-Route.get('contact', 'ContactController.index')
-Route.post('contact', 'ContactController.verify')
+Route.on('contact').render('contact')
+Route.post('contact', 'ContactController.send')
 
 /**
  |--------------------------------------------------------------------------
@@ -76,65 +78,75 @@ Route.post('contact', 'ContactController.verify')
  */
 
 Route.group(() => {
-    Route.get('login', 'LoginController.create')
-    Route.post('login', 'LoginController.store')
-    Route.get('register', 'RegisterController.create')
-    Route.post('register', 'RegisterController.store')
+    Route.on('login').render('auth.login')
+    Route.post('login', 'AuthController.login')
+    Route.on('register').render('auth.register')
+    Route.post('register', 'AuthController.register')
 }).middleware(['verif']);
 
 Route.group(() => {
-    Route.get('/me/client', 'ClientController.client')
-    Route.post('/me/client', 'ClientController.send')
+    Route.get('me/client', 'ClientController.client_request')
+    Route.post('me/client', 'ClientController.request')
 }).middleware(['auth','client']);
+
 Route.group(() => {
-    Route.get('/me/client/dashboard', 'ClientController.dashboard')
-    Route.post('/me/client/pay', 'ClientController.pay')
-    Route.get('/me/client/success', 'ClientController.paySuccess')
+    Route.get('me/client/dashboard', 'ClientController.dashboard')
+    Route.post('me/client/pay', 'ClientController.pay')
+    Route.get('success', 'ClientController.paypalSuccess')
+    Route.get('cancel', 'ClientController.paypalCancel')
 }).middleware(['auth','client_d']);
 
 Route.group(() => {
-    Route.get('/me/', 'BackofficeController.index')
-    Route.get('/me/profil', 'ProfilController.index')
+    Route.on('me/').render('dashboard.accueil')
+    Route.on('me/profil').render('dashboard.profil')
 
     Route.get('discord/callback', 'ProfilController.discordCallback')
 
-    Route.post('/me/profil/compte', 'ProfilController.storeBasic')
-    Route.post('/me/profil/infos', 'ProfilController.storeInfos')
+    Route.post('me/profil/compte', 'ProfilController.storeBasic')
+    Route.post('me/profil/infos', 'ProfilController.storeInfos')
 
-    Route.get('/legout', 'RegisterController.legout')
+    Route.get('logout', 'RegisterController.logout')
 }).middleware(['auth']);
 
 Route.group(() => {
-    Route.get('/me/comments', 'BackofficeController.comments')
-    Route.get('/me/comments/:id/delete', 'BackofficeController.destroy_comment')
-    Route.get('/me/comments/:id/valide', 'BackofficeController.valide_comment')
+    Route.get('me/comments', 'BackofficeController.comments')
+    Route.get('me/comments/:id/delete', 'BackofficeController.destroy_comment')
+    Route.get('me/comments/:id/valide', 'BackofficeController.valide_comment')
 
-    Route.get('/me/articles', 'BackofficeController.articles')
+    Route.get('me/articles', 'BackofficeController.articles')
 }).middleware(['auth','modo']);
 
 Route.group(() => {
-    Route.get('/me/write', 'PostController.create')
-    Route.post('/me/write', 'PostController.store')
+    Route.get('me/write', 'BlogController.create')
+    Route.post('me/write', 'BlogController.store')
 
-    Route.get('/me/write/:id-:url', 'PostController.edit')
-    Route.post('/me/write/:id-:url', 'PostController.update')
+    Route.get('me/write/:id-:url', 'BlogController.edit')
+    Route.post('me/write/:id-:url', 'BlogController.update')
 
-    Route.get('/blog/delete/:id-:url', 'PostController.delete')
+    Route.get('blog/delete/:id-:url', 'BlogController.delete')
     
-    Route.get('/me/upload', 'BackofficeController.upload')
+    Route.on('me/upload').render('dahsboard.upload')
 }).middleware(['auth','writer']);
 
 Route.group(() => {
-    Route.get('/admin/clients', 'AdminController.clients')
-    Route.get('/admin/users', 'AdminController.users')
-    Route.get('/admin/newsletter', 'AdminController.newsletter')
+    Route.get('admin/clients', 'ClientController.clients')
+    Route.get('admin/clients/:id', 'ClientController.show')
+    // Route.get('admin/users', 'AdminController.users')
+    // Route.get('admin/newsletter', 'AdminController.newsletter')
 
-    Route.get('/admin/portfolio', 'PortfolioController.create')
-    Route.post('/admin/portfolio', 'PortfolioController.store')
+    Route.get('admin/portfolio', 'PortfolioController.create')
+    Route.post('admin/portfolio', 'PortfolioController.store')
 
-    Route.get('/admin/portfolio/:id', 'PortfolioController.edit')
-    Route.post('/admin/portfolio/:id', 'PortfolioController.update')
-    Route.post('/admin/portfolio/:id/details', 'PortfolioController.updateDetails')
+    Route.get('admin/portfolio/:id', 'PortfolioController.edit')
+    Route.post('admin/portfolio/:id', 'PortfolioController.update')
+    Route.post('admin/portfolio/:id/details', 'PortfolioController.updateDetails')
 
-    Route.get('/admin/portfolio/:id/upgrade', 'PortfolioController.upgrade')
+    Route.get('admin/portfolio/:id/upgrade', 'PortfolioController.upgrade')
+    Route.get('admin/portfolio/:id/decline', 'PortfolioController.decline')
 }).middleware(['auth','admin']);
+
+/**
+ |--------------------------------------------------------------------------
+ | Error pages
+ |--------------------------------------------------------------------------
+ */
