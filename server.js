5c824834e072238433b0ecab23ef6964a7da0555
DraftMan <nicovanaarsen@gmail.com>
2018-08-18 00:26:11 +0200

    server with https

--- server.js
+++ server.js
@@ -18,8 +18,23 @@
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
+const https = require('https')
+const pem = require('pem')
 
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
+pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
+  if (error) {
+    return console.log(error)
+  }
+
+  const options = {
+    key: keys.serviceKey,
+    cert: keys.certificate
+  }
+
+  new Ignitor(require('@adonisjs/fold'))
+    .appRoot(__dirname)
+    .fireHttpServer((handler) => {
+      return https.createServer(options, handler)
+    })
+    .catch(console.error)
+})
