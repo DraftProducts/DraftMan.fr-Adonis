9a13b0f472293702666c2c116c041a9b6fea3561
DraftMan <nicovanaarsen@gmail.com>
2018-09-18 14:12:13 +0200

    custom error messages ajoutés

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -4,11 +4,17 @@ const BaseExceptionHandler = use('BaseExceptionHandler')
 
 class ExceptionHandler extends BaseExceptionHandler {
 
-  async handle (error, { request, response, session, view }) {
+  async handle (error, { request,response, session,view }) {
     if (error.code === 'E_INVALID_SESSION') {
       session.flash({ error: 'Veuillez vous reconnecter !' })
 
       return response.redirect('/login')
+    }
+    if (error.code === 'E_MISSING_DATABASE_ROW') {
+      return response.send(view.render('errors.404'))
+    }
+    if (error.name === 'HttpException'){
+      return response.send(view.render(`errors.${error.status}`))
     }
 
     return super.handle(...arguments)
