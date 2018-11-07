91870c148b1428aafedd30a222e57a396d078a9e
DraftMan <nicovanaarsen@gmail.com>
2018-11-07 19:09:53 +0100

    page created

--- app/Controllers/Http/DraftBotController.js
+++ app/Controllers/Http/DraftBotController.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const { get, post } = require('snekfetch');
+const lodash = require('lodash');
+
 class DraftBotController {
 
-  async index({ view }) {
+  async show({ view }) {
     return view.render('draftbot.index')
+  }
+
+  async index({ view,response }) {
+    const res = await get(`http://draftbot.draftman.fr/api/commands`)
+    const commands = lodash.flatten(Object.values(res.body.commands))
+    return view.render('draftbot.commands',{commands})
   }
 
   async invite({ response }) {
