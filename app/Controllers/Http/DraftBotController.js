5a38b1e0c302a159ab1c40120ddbe88ba2a801eb
DraftMan <nicovanaarsen@gmail.com>
2019-02-05 22:51:06 +0100

    bot removed

--- app/Controllers/Http/DraftBotController.js
+++ app/Controllers/Http/DraftBotController.js
@@ -1,22 +1,17 @@
 'use strict'
 
-const { get, post } = require('snekfetch');
-const lodash = require('lodash');
-
 class DraftBotController {
 
   async show({ view }) {
-    return view.render('draftbot.index')
+    response.redirect('https://www.draftbot.fr/',false, 301)
   }
 
-  async index({ view,response }) {
-    const res = await get(`http://draftbot.draftman.fr/api/commands`)
-    const commands = lodash.flatten(Object.values(res.body.commands))
-    return view.render('draftbot.commands',{commands})
+  async index({ response }) {
+    response.redirect('https://www.draftbot.fr/commandes', false, 301)
   }
 
   async invite({ response }) {
-    response.redirect('https://discordapp.com/oauth2/authorize?client_id=318312854816161792&scope=bot&permissions=1610083521')
+    response.redirect('https://www.draftbot.fr/invite', false, 301)
   }
 }
 
